0146ee64c183c09783e5d6995879224d2fa84734
Andrew Becker <absdsgn@gmail.com>
2018-07-14 18:58:33 -0400

    header cta

--- wp-content/themes/Moffitt/template-parts/header/header-image.php
+++ wp-content/themes/Moffitt/template-parts/header/header-image.php
@@ -10,11 +10,22 @@
 
 ?>
 <div class="custom-header">
-
-		<div class="custom-header-media">
-			<?php the_custom_header_markup(); ?>
-		</div>
+	<div class="header-cta">
+		<h1>
+			<?php echo get_post_meta($post->ID, 'CTA Header', true ); ?>
+		</h1>
+		<h2>
+			<?php echo get_post_meta($post->ID, 'CTA Text', true ); ?>
+		</h2>
+		<a href="<?php echo get_post_meta($post->ID, 'CTA URL', true ); ?>" class="header-cta-button">
+			<?php echo get_post_meta($post->ID, 'CTA Button', true ); ?>
+		</a>
+	</div>
+	<div class="custom-header-media">
+		<?php the_custom_header_markup(); ?>
+	</div>
 
 	<?php get_template_part( 'template-parts/header/site', 'branding' ); ?>
-
+	<div class="header-background-overlay" style="position: absolute; left: 0; top: 0; background: rgba(0, 0, 0, .35); width: 100%; height: 100%;">
+	</div>
 </div><!-- .custom-header -->
